f454ce5468472eecd1a6e50c6e2c3d0f3c12dec1
MARCOS VINÍCIUS GONÇALVEZ QUEIROZ <marcos.queiroz@e239.local>
2026-03-09 13:53:24 -0300

    Templates

---
+++ aula-02-templates-e-static-files/app.py
@@ -0,0 +1,46 @@
+#comentario no python 
+#importando o flask para a aplicação
+#de 'flask' (o pacote instalado) importar a classe 'Flask', a partir daí da pra usar todos os métodos (funções) do flask
+from flask import Flask, render_template
+#carregando(instanciar) o Flask na variavel app
+app = Flask(__name__, template_folder='views')
+#variaveis com 2 underlines são variáveis de ambiente do python (variaveis já criadas)
+#__name__ representa o nome da aplicação, fala o arquivo principal, ou seja = app = Flask(app.py)
+
+#criando a rota principal do site 
+@app.route('/')
+#def cria funções no python 
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/games')
+def games():
+    #criando variaveis para rota de games
+    titulo = "Portal 2"
+    ano = 2011
+    categoria = "Puzzle"
+    jogadores = ['Marcos', 'Richard', 'Miguel', 'Renato', 'Pedro']
+    return render_template('games.html', titulo = titulo, ano = ano, categoria = categoria, jogadores=jogadores)
+    #variavel do html = valor é a variavel do python
+    
+    
+@app.route('/consoles')
+def consoles():
+    # criando um objeto (dicionario)
+    #lista (array, vetor)
+    console = {"Nome":"Playstation 2",
+               "Fabricante":"Sony",
+               "Ano":2000}
+    return render_template('consoles.html',console=console)
+
+
+
+
+
+#iniciando servidor na porta 5000
+if __name__ == '__main__':
+#verificando se o arquivo gravado em __name__ é o arquivo principal
+    app.run(port=5000, debug=True)
+#O método .run() inicia o servidor
